9bf728a8af0b4760243d8d84efc1e64a4b3103a2
Hyde <john.krotov@gmail.com>
2023-01-26 19:28:12 +0100

    Threading on

--- async.cpp
+++ async.cpp
@@ -11,54 +11,97 @@
 #include <mutex>
 #include <condition_variable>
 #include <sstream>
+#include <queue>
+#include <atomic>
 
 namespace async {
 
 using namespace std;
 
+struct Bulk {
+    vector<string> commands;
+    time_t block_time;
+};
+
 class IPrinter {
 protected:
-    void print_to(ostream& stream, const vector<string>& bulk) {
+    std::queue<Bulk> q;
+    std::mutex m;
+    std::condition_variable cv;
+    bool stopped = false;
+
+    void print_to(ostream& stream, const Bulk& bulk) {
         stream << "bulk: ";
-        for (int i = 0; i < bulk.size(); i++) {
-            stream << bulk[i];
-            if (i != bulk.size()-1) {
+        for (int i = 0; i < bulk.commands.size(); i++) {
+            stream << bulk.commands[i];
+            if (i != bulk.commands.size()-1) {
                 stream << ", ";
             }
         }
         stream << endl;
     }
+
 public:
-    virtual void print(const vector<string>& bulk, time_t block_time) = 0;
+    virtual void print(const Bulk& bulk) = 0;
+    void worker() {
+        while (true) {
+            std::unique_lock<std::mutex> lock(m);
+            cv.wait(lock, [this](){ return !q.empty() || stopped; });
+            if (stopped) {
+                return;
+            }
+            Bulk bulk = q.front();
+            q.pop();
+            lock.unlock();
+            // Process task
+            print(bulk);
+        }
+    }
+    std::thread spawn() {
+        return std::thread(&IPrinter::worker, this);
+    }
+    void stop_work() {
+        while (!q.empty()) {
+            this_thread::sleep_for(chrono::seconds(1));
+        }
+        stopped = true;
+        cv.notify_all();
+    }
+    void add_task(Bulk& bulk) {
+        std::unique_lock<std::mutex> lock(m);
+        q.push(bulk);
+        lock.unlock();
+        cv.notify_one();
+    }
 };
 
 class ConsolePrinter : public IPrinter {
 public:
-    void print(const vector<string>& bulk, time_t block_time) {
+    void print(const Bulk& bulk) {
         print_to(cout, bulk);
     }
 };
 
 class FilePrinter : public IPrinter {
+    std::atomic_int id = 0;
 public:
-    void print(const vector<string>& bulk, time_t block_time) {
+    void print(const Bulk& bulk) {
         ofstream file;
-        file.open("bulk" + to_string(block_time) + ".log");
+        file.open("bulk" + to_string(bulk.block_time) + "_" + to_string(id++) + ".log");
         print_to(file, bulk);
         file.close();
     }
 };
 
 class PacketHandler {
-    const int N = 3;
-    vector<string> bulk;
+    const size_t N;
+    Bulk bulk;
     int dyn_block_nesting = 0;
-    std::time_t block_time;
     list<IPrinter*> printers;
 public:
-    PacketHandler() = default;
-    ~PacketHandler() {
-        cout << "Packet handler dctr" << endl;
+    PacketHandler(size_t N) : N(N) {}
+    bool is_zero_nesting() {
+        return true ? dyn_block_nesting == 0 : false;
     }
     void add_packet(string line) {
         if (line == "{") {
@@ -72,29 +115,20 @@ public:
                 pop_print();
             }
         } else {
-            if (bulk.empty()) {
-                block_time = time(nullptr);
+            if (bulk.commands.empty()) {
+                bulk.block_time = time(nullptr);
             }
-            bulk.push_back(line);
-            if (dyn_block_nesting == 0 && bulk.size() == N) {
+            bulk.commands.push_back(line);
+            if (dyn_block_nesting == 0 && bulk.commands.size() == N) {
                 pop_print();
             }
         }
     }
 
     void pop_print() {
-        if (!bulk.empty()) {
+        if (!bulk.commands.empty()) {
             notify();
-            bulk.clear();
-        }
-    }
-
-    void process() {
-        for (string line; getline(cin, line);) {
-            add_packet(line);
-        }
-        if (dyn_block_nesting == 0) {
-            pop_print();
+            bulk.commands.clear();
         }
     }
 
@@ -116,22 +150,31 @@ public:
 
     void notify() {
         for (auto printer : printers) {
-            printer->print(bulk, block_time);
+            //printer->print(bulk);
+            printer->add_task(bulk);
         }
     }
 };
 
 class HandleDispatch {
 private:
-    std::mutex mutex_;
+    std::mutex mtx;
+    bool stopped = false;
     ConsolePrinter cp;
     FilePrinter fp;
+    thread w1, w2, w3;
     HandleDispatch()
     {
-        cout << "Handle dispatch ctr" << endl;
+        w1 = cp.spawn();
+        w2 = fp.spawn();
+        w3 = fp.spawn();
     }
     ~HandleDispatch() {
-        cout << "Handle dispatch dctr" << endl;
+        cp.stop_work();
+        fp.stop_work();
+        w1.join();
+        w2.join();
+        w3.join();
     }
 public:
     HandleDispatch(HandleDispatch &other) = delete;
@@ -142,8 +185,9 @@ public:
         return instance;
     }
     PacketHandler* create_handler(size_t bulk) {
-        auto ph = new PacketHandler();
+        auto ph = new PacketHandler(bulk);
         ph->attach(&cp);
+        ph->attach(&fp);
         return ph;
     }
 
@@ -153,39 +197,26 @@ public:
     }
 
     void destroy_handler(PacketHandler* ph) {
-        if (dyn_block_nesting == 0) {
-            //pop_print();
+        if (ph->is_zero_nesting()) {
+            ph->pop_print();
         }
         delete ph;
     }
 };
 
-int notmain() {
-    PacketHandler ph;
-    ConsolePrinter cp;
-    FilePrinter fp;
-    ph.attach(&cp);
-    ph.attach(&fp);
-    ph.process();
-    return 0;
-}
-
 handle_t connect(std::size_t bulk) {
-    std::cout << "Connect\n";
     auto& hd = HandleDispatch::get_instance();
-    auto ph = hd.create_handler(3);
+    auto ph = hd.create_handler(bulk);
     
     return ph;
 }
 
 void receive(handle_t handle, const char *data, std::size_t size) {
-    std::cout << "Receive\n";
     auto& hd = HandleDispatch::get_instance();
     hd.execute_handler(static_cast<PacketHandler*>(handle), data);
 }
 
 void disconnect(handle_t handle) {
-    std::cout << "Disconnect\n";
     auto& hd = HandleDispatch::get_instance();
     hd.destroy_handler(static_cast<PacketHandler*>(handle));
 }
